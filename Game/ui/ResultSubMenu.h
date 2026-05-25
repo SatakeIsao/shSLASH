1cda07fb0e478f1e3e9b8ffd75a8ac179dabed61
ItouYusei <CA01244004@st.kawahara.ac.jp>
2026-05-25 17:57:08 +0900

    リザルト画面の数字アニメーション変更/メニュー遷移実装

--- Game/ui/ResultSubMenu.h
+++ Game/ui/ResultSubMenu.h
@@ -13,6 +13,7 @@ namespace app {
             void OnOpen(); 
 
         private:
+            void StartFadeIn();
             // 今回操作したいUIのポインタ
             UIIcon* backFilter_ = nullptr;
             UIIcon* menuLine1_ = nullptr;
@@ -22,6 +23,20 @@ namespace app {
             UIIcon* title_ = nullptr;
             UIIcon* exit_ = nullptr;
             UIIcon* highlight_ = nullptr;
+
+            int cursorIndex_ = 0;
+            bool isReturnTitleDecided_ = false;
+            bool isRetryDecided_ = false;
+            bool isExitDecided_ = false;
+
+            int waitFrame_ = 0;
+            float fadeProgress_ = 0.0f;
+
+            Vector3 cursorPositions_[3];
+        public:
+            bool IsReturnTitleDecided() const { return isReturnTitleDecided_; }
+            bool IsRetryDecided() const { return isRetryDecided_; }
+            bool IsExitDecided() const { return isExitDecided_; }
         };
     }
 }
